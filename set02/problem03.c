a6ba8ca0c806dde2b12e1af9414b1215460eacfb
Phalguni  S P <114557138+phalgunisp@users.noreply.github.com>
2023-01-05 09:03:06 +0000

    problem solved

--- set02/problem03.c
+++ set02/problem03.c
@@ -18,3 +18,26 @@ int input_number(){
   scanf("%d", &number);
   return number;
 }
+
+int is_composite(int n){
+  int i,result;
+  for(i=1;i<=n;i++){
+    if(n%i==0){
+      result=result+1;
+    }
+   
+    }
+  return result;
+}
+
+void output(int n, int result){
+  if(result==2){
+     printf("%d is not a composite number/ It is a prime number.",n);
+ }
+  else if (result>2){
+    printf("%d is a composite number.",n);
+  }
+  else{
+    printf("1 is a unique number");
+  }
+}
