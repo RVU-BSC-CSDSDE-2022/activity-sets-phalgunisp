e1d612c3c0d67054b4594f24bf291b9384f60340
Phalguni  S P <114557138+phalgunisp@users.noreply.github.com>
2023-01-10 06:08:55 +0000

    problem solved

--- set02/problem05.c
+++ set02/problem05.c
@@ -5,3 +5,33 @@
 int input();
 int find_gcd(int a, int b);
 void output(int a, int b, int gcd);
+
+int main(){
+  int a,b,gc_d;
+  a=input();
+  b=input();
+  gc_d=find_gcd(a,b);
+  output(a,b,gc_d);
+  return 0;
+}
+
+int input(){
+  int number;
+  printf("Enter the number: \n");
+  scanf("%d", &number);
+  return number;
+}
+
+int find_gcd(int a, int b){
+  int gcd;
+  for(int i=1;i<=a && i<=b;i++){
+    if(a%i==0 && b%i==0){
+      gcd=i;
+    }
+  }
+  return gcd;
+}
+
+void output(int a, int b, int gcd){
+  printf("%d",gcd);
+}
---
+++ set02/problem06.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+//Write a program to reverse a string.
+
+void input_string(char *a);
+void str_reverse(char *str, char *rev_str);
+void output(char *a, char *reverse_a);
+
+int main(){
+  int a,str,
+}
